4db3802b020d88a00621a01a55d5039e2e577bee
sydbiscuit <93106878+sydbiscuit@users.noreply.github.com>
2021-12-07 14:24:05 -0800

    Controls 2 motors powering on for 0.5s and off for 1s

--- PWM Go No Go/src/main.cpp
+++ PWM Go No Go/src/main.cpp
@@ -9,14 +9,16 @@ void setup()
  // Attach the ESC signal wire six pins up from the bottom of CN9.
   // make that pin an output PWM signal.
 
-  pinMode(PB4, OUTPUT);  // timer 3 channel 1 
+  pinMode(PC7, OUTPUT);  // timer 3 channel 1 
+  pinMode(PA9, OUTPUT);  // timer 3 channel 1 
   
 
   Serial.begin(115200);
   analogWriteResolution(8);    // set 8-bit timer channel
 
   potValue = 13;
-  analogWrite(PB4, potValue);
+  analogWrite(PC7, potValue);
+  analogWrite(PA9, potValue);
   analogWriteFrequency(50);
   Serial <<  "first time " << potValue << endl;
   delay(1); 
@@ -26,15 +28,18 @@ void setup()
 void loop() 
 {
   potValue = 13;
-  analogWrite(PB4, potValue);
+  analogWrite(PC7, potValue);
+  analogWrite(PA9, potValue);
   analogWriteFrequency(50);
   Serial <<  potValue << endl;
-  delay(10000);
+  delay(1000);
 
   potValue = 17;                       // potValue(13) stop signal, potValue (26) max rpm
-  analogWrite(PB4, potValue);    // potValue(10) sends a 1ms pulse width and is needed to send a stop signal to the ESC. This value needs to be present when the ESC is plugged in
+  // potValue(10) sends a 1ms pulse width and is needed to send a stop signal to the ESC. This value needs to be present when the ESC is plugged in
+  analogWrite(PC7, potValue);
+  analogWrite(PA9, potValue);
   analogWriteFrequency(50);  //50 Hz freq 
   Serial <<  potValue << endl;
-  delay(5000);
+  delay(500);
 
 }
